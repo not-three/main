bc4a6da80e11549c3821678d6476a78bce4afe47
scolastico <scolastico@users.noreply.github.com>
2024-10-19 19:39:15 +0000

    Remove preview for PR 6 🛬

--- pr-preview/pr-6/_nuxt/D-9kdkgH.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,v as s,E as o,t as a}from"./CBK0DoRF.js";const c={},n={class:"base-container"};function r(e,_){return a(),s("div",n,[o(e.$slots,"default")])}const d=t(c,[["render",r]]);export{d as default};
