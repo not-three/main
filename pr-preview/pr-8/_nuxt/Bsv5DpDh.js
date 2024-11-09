639f643a42a8f992b80edd64f25bae77bcbc23ea
scolastico <scolastico@users.noreply.github.com>
2024-11-09 19:51:37 +0000

    Deploy preview for PR 8 🛫

---
+++ pr-preview/pr-8/_nuxt/Bsv5DpDh.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./C9pP3LHp.js";import"./ofoD0NTt.js";import"./BFHdUEn0.js";import"./zgIcYQv5.js";import"./C3RZYoGz.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.46.0(21007360cad28648bdf46282a2592cb47c3a7a6f)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var n=t,c={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{n as conf,c as language};
